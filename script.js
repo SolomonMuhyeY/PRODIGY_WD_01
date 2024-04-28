8e8e40d2567835acf600f7d14bc5e6d04b24a55f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-28 19:14:41 +0300

    removing unnecessary comment

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener("scroll", function () {
   var header = document.getElementById("nav__container");
   if (window.scrollY > 0) {
-    header.classList.add("scrolled"); // Add a CSS class to change the background color
+    header.classList.add("scrolled");
   } else {
-    header.classList.remove("scrolled"); // Remove the CSS class when scrolled to the top
+    header.classList.remove("scrolled"); 
   }
 });
 // toggler
