55d056131c786c32624e4aba2cc0ef10c44ff3d2
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-29 00:56:32 +0300

    removing unused code

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.addEventListener("scroll", function () {
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
   } else {
-    header.classList.remove("scrolled"); 
   }
 });
 // toggler
