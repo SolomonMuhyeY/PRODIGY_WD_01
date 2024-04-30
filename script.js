43d5a30a96bf82d8103de3f782fb519260ef6fef
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-30 09:38:36 +0300

    adding some functionality for nav

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 window.addEventListener("scroll", function () {
   var header = document.getElementById("nav__container");
-  // if (window.scrollY > 0) {
-  //   header.classList.add("scrolled");
-  // } 
 });
 // toggler
 const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenu = document.querySelector(".mobile__nav");
 const navLinks = document.querySelector(".mobile__nav");
-
 hamburgerBtn.addEventListener("click", () => {
   if (navLinks.style.display === "block") {
     navLinks.style.display = "none";
   } else {
     navLinks.style.display = "block";
+  }
+});
+document.addEventListener("click", (e) => {
+  if (!mobileMenu.contains(e.target) && e.target !== hamburgerBtn) {
+    mobileMenu.style.display = "none";
   }
 });
